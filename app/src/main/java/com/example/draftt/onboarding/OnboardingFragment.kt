38ff2963d028db6627bde493931ea6369e86dcc6
Adil Mian <adilmian95@gmail.com>
2021-03-17 00:01:50 -0400

    setup navigation graph

--- app/src/main/java/com/example/draftt/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/example/draftt/onboarding/OnboardingFragment.kt
@@ -1,7 +1,25 @@
 package com.example.draftt.onboarding
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.draftt.databinding.OnboardingFragmentLayoutBinding
 
 class OnboardingFragment : Fragment() {
-    // hello world
+
+    lateinit var binding: OnboardingFragmentLayoutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+
+        binding = OnboardingFragmentLayoutBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
 }
