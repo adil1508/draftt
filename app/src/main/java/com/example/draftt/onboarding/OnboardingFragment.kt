9a0adaf07aea72de6daa00474dd638adef86b194
Adil Mian <adilmian95@gmail.com>
2021-03-17 20:25:08 -0400

    Added viewpager in onboarding layout

--- app/src/main/java/com/example/draftt/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/example/draftt/onboarding/OnboardingFragment.kt
@@ -5,11 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.draftt.databinding.OnboardingFragmentLayoutBinding
 
 class OnboardingFragment : Fragment() {
 
     lateinit var binding: OnboardingFragmentLayoutBinding
+
+    lateinit var viewPager: ViewPager2
+
+    private val ONBOARDING_SLIDES = 3
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,10 +25,12 @@ class OnboardingFragment : Fragment() {
 
         binding = OnboardingFragmentLayoutBinding.inflate(inflater, container, false)
 
+        // setup viewpager2
+        viewPager = binding.onboardingViewPager2
+
         return binding.root
     }
 
-    // TODO: Put the ViewPager2 as the layout for this fragment
     // TODO: implemented the FragmentStateAdapter here
     // TODO: Need to figure out how to get the slides going
 }
