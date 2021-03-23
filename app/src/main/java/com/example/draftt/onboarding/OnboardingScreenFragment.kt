1bf0d3eadbdc1657e13fff4aadf72cb1aca87437
Adil Mian <adilmian95@gmail.com>
2021-03-22 20:56:57 -0400

    setup button handling for onboarding slide 3

--- app/src/main/java/com/example/draftt/onboarding/OnboardingScreenFragment.kt
+++ app/src/main/java/com/example/draftt/onboarding/OnboardingScreenFragment.kt
@@ -1,15 +1,18 @@
 package com.example.draftt.onboarding
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.draftt.auth.LoginActivity
 import com.example.draftt.databinding.OnboardingSlide1Binding
 import com.example.draftt.databinding.OnboardingSlide2Binding
 import com.example.draftt.databinding.OnboardingSlide3Binding
+import timber.log.Timber
 
-class OnboardingScreenFragment(val position: Int) : Fragment() {
+class OnboardingScreenFragment(private val position: Int) : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,8 +22,21 @@ class OnboardingScreenFragment(val position: Int) : Fragment() {
         return when (position) {
             0 -> OnboardingSlide1Binding.inflate(inflater, container, false).root
             1 -> OnboardingSlide2Binding.inflate(inflater, container, false).root
-            2 -> OnboardingSlide3Binding.inflate(inflater, container, false).root
+            2 -> {
+                val binding: OnboardingSlide3Binding =
+                    OnboardingSlide3Binding.inflate(inflater, container, false)
+                binding.letsDrafttButton.setOnClickListener {
+                    startLoginActivity()
+                }
+                binding.root
+            }
             else -> super.onCreateView(inflater, container, savedInstanceState)
         }
+    }
+
+    private fun startLoginActivity() {
+        Timber.d("Starting Login Activity")
+        val intent = Intent(activity, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
