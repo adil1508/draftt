6d492c20a498185dd62bfbc8314fb32fc4e69225
Adil Mian <adilmian95@gmail.com>
2021-03-22 20:00:41 -0400

    import cleanup

--- app/src/main/java/com/example/draftt/onboarding/OnboardingPagerFragment.kt
+++ app/src/main/java/com/example/draftt/onboarding/OnboardingPagerFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.widget.ViewPager2
 import com.example.draftt.databinding.OnboardingFragmentLayoutBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
