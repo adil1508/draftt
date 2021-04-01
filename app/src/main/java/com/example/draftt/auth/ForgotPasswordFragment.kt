acb841f83a49d3e78e366c75ac9da1ce0764898b
Adil Mian <adilmian95@gmail.com>
2021-03-31 22:05:56 -0400

    added empty forget password fragment + layout file

---
+++ app/src/main/java/com/example/draftt/auth/ForgotPasswordFragment.kt
@@ -0,0 +1,28 @@
+package com.example.draftt.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.draftt.R
+
+class ForgotPasswordFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            ForgotPasswordFragment()
+    }
+}
