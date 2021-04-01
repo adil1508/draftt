1dca5666461abc7352ec4a573c7ef5d5436c37e0
Adil Mian <adilmian95@gmail.com>
2021-03-31 21:52:45 -0400

    added skeleton for clickable span

--- app/src/main/java/com/example/draftt/auth/LoginFragment.kt
+++ app/src/main/java/com/example/draftt/auth/LoginFragment.kt
@@ -1,6 +1,8 @@
 package com.example.draftt.auth
 
 import android.os.Bundle
+import android.text.TextPaint
+import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,6 +19,32 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = LoginFragmentLayoutBinding.inflate(inflater, container, false)
+        setupClickableSpans()
         return binding.root
+    }
+
+    private fun setupClickableSpans() {
+        TODO("Implement")
+    }
+
+    inner class CustomClickableSpan(private val clickableTextType: ClickableTextType) :
+        ClickableSpan() {
+
+        override fun updateDrawState(ds: TextPaint) {
+            super.updateDrawState(ds)
+            ds.isUnderlineText = false
+        }
+
+        override fun onClick(widget: View) {
+            when (clickableTextType) {
+                ClickableTextType.FORGET_PASSWORD -> TODO("Go to Forget password fragment")
+                ClickableTextType.SIGN_UP -> TODO("Go to Sign up fragment")
+            }
+        }
+    }
+
+    enum class ClickableTextType {
+        FORGET_PASSWORD,
+        SIGN_UP
     }
 }
