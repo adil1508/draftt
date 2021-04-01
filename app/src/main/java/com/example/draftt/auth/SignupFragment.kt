a77effb47eb561fdc91f911b2e87e94ecae5c526
Adil Mian <adilmian95@gmail.com>
2021-03-31 22:19:26 -0400

    added signup skeleton

---
+++ app/src/main/java/com/example/draftt/auth/SignupFragment.kt
@@ -0,0 +1,27 @@
+package com.example.draftt.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.draftt.databinding.FragmentSignupBinding
+
+class SignupFragment: Fragment() {
+
+    lateinit var binding: FragmentSignupBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSignupBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = SignupFragment()
+    }
+}
