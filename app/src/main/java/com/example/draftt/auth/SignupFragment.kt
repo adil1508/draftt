407232160d4937ee4fc332ab62a19296d486acb2
Adil Mian <adilmian95@gmail.com>
2021-04-07 23:11:09 -0400

    handled navigation from Login text on signup screen

--- app/src/main/java/com/example/draftt/auth/SignupFragment.kt
+++ app/src/main/java/com/example/draftt/auth/SignupFragment.kt
@@ -1,11 +1,19 @@
 package com.example.draftt.auth
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.draftt.R
 import com.example.draftt.databinding.FragmentSignupBinding
+import timber.log.Timber
 
 class SignupFragment : Fragment() {
 
@@ -15,9 +23,38 @@ class SignupFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentSignupBinding.inflate(inflater, container, false)
+        setupClickableSpans()
         return binding.root
+    }
+
+    private fun setupClickableSpans() {
+
+        val loginTextSpannable = SpannableString(binding.loginText.text)
+        loginTextSpannable.setSpan(
+            CustomClickableSpan(),
+            binding.loginText.text.indexOf(getString(R.string.login_text)),
+            binding.loginText.text.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        binding.loginText.text = loginTextSpannable
+        binding.loginText.movementMethod = LinkMovementMethod.getInstance()
+        
+    }
+
+    inner class CustomClickableSpan : ClickableSpan() {
+
+        override fun updateDrawState(ds: TextPaint) {
+            super.updateDrawState(ds)
+            ds.isUnderlineText = false
+            ds.color = resources.getColor(R.color.orange_500, requireActivity().theme)
+        }
+
+        override fun onClick(widget: View) {
+            Timber.d("Login string tapped")
+            findNavController().navigate(SignupFragmentDirections.actionSignupFragmentToLoginFragment())
+        }
     }
 
     companion object {
