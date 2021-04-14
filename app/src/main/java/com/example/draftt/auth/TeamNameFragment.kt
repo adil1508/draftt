166d0c64f6deba2665d2f18f541079a7ff105f5e
Adil Mian <adilmian95@gmail.com>
2021-04-13 21:52:17 -0400

    added fragment

---
+++ app/src/main/java/com/example/draftt/auth/TeamNameFragment.kt
@@ -0,0 +1,28 @@
+package com.example.draftt.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.draftt.databinding.FragmentTeamnameBinding
+
+class TeamNameFragment : Fragment() {
+
+    private lateinit var binding: FragmentTeamnameBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentTeamnameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+
+        fun newInstance() = TeamNameFragment()
+
+    }
+}
