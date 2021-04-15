5eb224f87522151eb9c35821d1b822d27b29cee6
Adil Mian <adilmian95@gmail.com>
2021-04-15 15:45:48 -0400

    skeletons are uppp

--- app/src/main/java/com/example/draftt/auth/repositories/FirebaseAuthentication.kt
+++
@@ -1,8 +0,0 @@
-package com.example.draftt.auth.repositories
-
-class FirebaseAuthentication {
-
-    init {
-        // This is where we'll initialize an instance of FirebaseAuth
-    }
-}
---
+++ app/src/main/java/com/example/draftt/auth/repositories/FirebaseAuthenticationService.kt
@@ -0,0 +1,43 @@
+package com.example.draftt.auth.repositories
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import timber.log.Timber
+
+// This class will be injected into FirebaseAuthRepository
+class FirebaseAuthenticationService {
+
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
+    }
+
+    fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Timber.d("Successfully logged in user with email: $email")
+            } else {
+                Timber.d("Failed to log in user with email: $email")
+            }
+        }
+    }
+
+    fun signup(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Timber.d("Successfully created user with email: $email")
+            } else {
+                Timber.d("FAILED to create user with email: $email")
+            }
+        }
+
+    }
+
+    fun currentUser(): FirebaseUser? {
+        return auth.currentUser
+    }
+
+    fun logout() = auth.signOut()
+
+}
