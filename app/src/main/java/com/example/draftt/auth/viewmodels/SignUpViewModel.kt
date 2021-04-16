1f051408672a015360898d870b4afa3245cfc2df
Adil Mian <adilmian95@gmail.com>
2021-04-15 23:21:05 -0400

    hooked up viewmodel to fragment

--- app/src/main/java/com/example/draftt/auth/viewmodels/SignUpViewModel.kt
+++ app/src/main/java/com/example/draftt/auth/viewmodels/SignUpViewModel.kt
@@ -1,16 +1,24 @@
 package com.example.draftt.auth.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.draftt.auth.repositories.FirebaseAuthRepository
 import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.launch
 
-class SignUpViewModel: ViewModel() {
+class SignUpViewModel : ViewModel() {
 
     private val authRepository by lazy {
         FirebaseAuthRepository()
     }
 
-    fun signup(email: String, password: String) = authRepository.signup(email, password)
+    fun signup(email: String, password: String) {
+        authRepository.signup(email, password)
+//        TODO: this is how we should be doing this
+//        viewModelScope.launch {
+//            authRepository.signup(email, password)
+//        }
+    }
 
     fun logout() = authRepository.logout()
 
