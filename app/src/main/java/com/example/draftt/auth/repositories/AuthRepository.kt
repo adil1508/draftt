54bf1f0e0b73202982cd2e29a3c415ef83c5cd0d
Adil Mian <adilmian95@gmail.com>
2021-04-15 23:19:07 -0400

    small comment

--- app/src/main/java/com/example/draftt/auth/repositories/AuthRepository.kt
+++ app/src/main/java/com/example/draftt/auth/repositories/AuthRepository.kt
@@ -2,6 +2,7 @@ package com.example.draftt.auth.repositories
 
 import com.google.firebase.auth.FirebaseUser
 
+// TODO: This interface seems really unnecessary
 interface AuthRepository {
     fun login(email: String, password: String)
     fun signup(email: String, password: String)
