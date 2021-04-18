1a3e84215814e83e5702d3117d893401d8847124
Adil Mian <adilmian95@gmail.com>
2021-04-18 13:20:37 -0400

    moved firebase service login in here

--- app/src/main/java/com/example/draftt/auth/repositories/FirebaseAuthRepository.kt
+++ app/src/main/java/com/example/draftt/auth/repositories/FirebaseAuthRepository.kt
@@ -1,27 +1,59 @@
 package com.example.draftt.auth.repositories
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import timber.log.Timber
 
-class FirebaseAuthRepository : AuthRepository {
+class FirebaseAuthRepository {
 
-    // TODO: Needs an instance of Firebase Auth, which should be injected
-    private val firebase by lazy {
-        FirebaseAuthService()
+    private val _authResult = MutableLiveData(
+        AuthResult(
+            status = null,
+            error = null
+        )
+    )
+    val authResult: LiveData<AuthResult>
+        get() {
+            return _authResult
+        }
+
+    // Firebase Auth Service
+    private val firebaseAuth: FirebaseAuth by lazy {
+        Firebase.auth
     }
 
-    override fun login(email: String, password: String) {
-        firebase.login(email, password)
+    fun login(email: String, password: String) {
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Timber.d("Successfully logged in user with email: $email")
+                _authResult.postValue(AuthResult(status = true, error = null))
+            } else {
+                Timber.d("Failed to log in user with email: $email")
+                _authResult.postValue(AuthResult(status = false, error = task.exception.toString()))
+            }
+        }
     }
 
-    override fun signup(email: String, password: String) {
-        firebase.signup(email, password)
+    fun signup(email: String, password: String) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Timber.d("Successfully created user with email: $email")
+                _authResult.postValue(AuthResult(status = true, error = null))
+            } else {
+                Timber.d("FAILED to create user with email: $email")
+                _authResult.postValue(AuthResult(status = false, error = task.exception.toString()))
+            }
+        }
+
     }
 
-    override fun currentUser(): FirebaseUser? {
-        return firebase.currentUser()
+    fun currentUser(): FirebaseUser? {
+        return firebaseAuth.currentUser
     }
 
-    override fun logout() {
-        firebase.logout()
-    }
+    fun logout() = firebaseAuth.signOut()
 }
