0948462b4b82a2e0200ab6c49a19e29408cecfe5
Adil Mian <adilmian95@gmail.com>
2021-04-18 14:23:56 -0400

    signup flow complete

--- app/src/main/java/com/example/draftt/auth/viewmodels/SignUpViewModel.kt
+++ app/src/main/java/com/example/draftt/auth/viewmodels/SignUpViewModel.kt
@@ -1,15 +1,23 @@
 package com.example.draftt.auth.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.draftt.auth.repositories.AuthResult
 import com.example.draftt.auth.repositories.FirebaseAuthRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class SignUpViewModel : ViewModel() {
 
+    val authResult: LiveData<AuthResult>
+
     private val authRepository by lazy {
         FirebaseAuthRepository()
+    }
+
+    init {
+        authResult = authRepository.authResult
     }
 
     fun signup(email: String, password: String) {
