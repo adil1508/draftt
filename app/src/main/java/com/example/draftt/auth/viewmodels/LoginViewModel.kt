d82f45189acbff9cd3ba118156e61c8d5ccd04f7
Adil Mian <adilmian95@gmail.com>
2021-04-18 13:29:52 -0400

    setup the livedata in viewmodel

--- app/src/main/java/com/example/draftt/auth/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/example/draftt/auth/viewmodels/LoginViewModel.kt
@@ -1,13 +1,22 @@
 package com.example.draftt.auth.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.draftt.auth.repositories.AuthResult
 import com.example.draftt.auth.repositories.FirebaseAuthRepository
 
 class LoginViewModel : ViewModel() {
 
+    val authResult: LiveData<AuthResult>
+
     // TODO: This should be injected
     private val authRepository by lazy {
         FirebaseAuthRepository()
+    }
+
+    init {
+        authResult = authRepository.authResult
     }
 
     fun login(email: String, password: String) {
