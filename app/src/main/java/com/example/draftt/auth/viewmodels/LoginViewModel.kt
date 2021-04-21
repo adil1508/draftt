8d31742037fed9594bd0ceee63fd02f4b594871a
Adil Mian <adilmian95@gmail.com>
2021-04-21 00:11:47 -0400

    injected repo into viewmodels -- it works!

--- app/src/main/java/com/example/draftt/auth/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/example/draftt/auth/viewmodels/LoginViewModel.kt
@@ -5,21 +5,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.draftt.auth.repositories.AuthResult
 import com.example.draftt.auth.repositories.FirebaseAuthRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class LoginViewModel : ViewModel() {
+@HiltViewModel
+class LoginViewModel @Inject constructor(private val authRepository: FirebaseAuthRepository) :
+    ViewModel() {
 
-    val authResult: LiveData<AuthResult>
-
-    // TODO: This should be injected
-    private val authRepository by lazy {
-        FirebaseAuthRepository()
-    }
-
-    init {
-        authResult = authRepository.authResult
-    }
+    val authResult: LiveData<AuthResult> = authRepository.authResult
 
     fun login(email: String, password: String) {
         viewModelScope.launch(Dispatchers.IO) {
