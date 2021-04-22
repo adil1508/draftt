ed4bc349d1848df74a28f5a091900086f1bb4882
Adil Mian <adilmian95@gmail.com>
2021-04-21 23:19:32 -0400

    fixed a few Hilt things

--- app/src/main/java/com/example/draftt/auth/views/AuthActivity.kt
+++ app/src/main/java/com/example/draftt/auth/views/AuthActivity.kt
@@ -3,7 +3,9 @@ package com.example.draftt.auth.views
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.draftt.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
