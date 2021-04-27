a771ad148525bcba33aecb0772fd8bb95b6438cb
Adil Mian <adilmian95@gmail.com>
2021-04-26 23:56:15 -0400

    add flow notes

--- app/src/main/java/com/aa/draftt/MainActivity.kt
+++ app/src/main/java/com/aa/draftt/MainActivity.kt
@@ -7,5 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+        * TODO:
+        *  - rename this activity to something like TrampolineActivity
+        *  - This will check if the user has opened the app for the very first time
+        *       - If so, setup the OnboardingNavigationGraph and set a bool in sharedpref that it's done
+        *       - else, route to AuthActivity
+        *  - AuthActivity will check if user is logged in or not
+        *       - If so, route to HomeActivity
+        *       - If not, setup the LoginNavigationGraph
+        *
+        * */
     }
 }
