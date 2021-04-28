dabceacab973fb13e38cb6dd0fb6594bbca1bf0c
Adil Mian <adilmian95@gmail.com>
2021-04-27 22:18:25 -0400

    renamed main acitivity

--- app/src/main/java/com/aa/draftt/RedHypergiantActivity.kt
+++ app/src/main/java/com/aa/draftt/RedHypergiantActivity.kt
@@ -3,7 +3,7 @@ package com.aa.draftt
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class RedHypergiantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
