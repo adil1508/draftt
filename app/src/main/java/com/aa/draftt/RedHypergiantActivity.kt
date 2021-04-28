ddcef5671cf958aa3bf7c6f6fc854b43634f8d91
Adil Mian <adilmian95@gmail.com>
2021-04-27 23:02:31 -0400

    hypergiant activity just routes to auth activity

--- app/src/main/java/com/aa/draftt/RedHypergiantActivity.kt
+++ app/src/main/java/com/aa/draftt/RedHypergiantActivity.kt
@@ -1,23 +1,23 @@
 package com.aa.draftt
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.aa.draftt.auth.views.AuthActivity
 
 class RedHypergiantActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         /*
-        * TODO:
-        *  - rename this activity to something like TrampolineActivity
-        *  - This will check if the user has opened the app for the very first time
-        *       - If so, setup the OnboardingNavigationGraph and set a bool in sharedpref that it's done
-        *       - else, route to AuthActivity
-        *  - AuthActivity will check if user is logged in or not
-        *       - If so, route to HomeActivity
-        *       - If not, setup the LoginNavigationGraph
-        *
-        * */
+        *   For now it just routes to the AuthActivity, which checks if a user is logged in or no
+        *       - If logged in, route to HomeActivity
+        *       - Else, setup the AuthNavGraph
+        */
+
+        val intent = Intent(this, AuthActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+        finish()
     }
 }
