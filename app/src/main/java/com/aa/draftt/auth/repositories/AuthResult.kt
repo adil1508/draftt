e950b23ebaca82d8e4b16085441a86135f422063
Adil Mian <adilmian95@gmail.com>
2021-04-28 23:47:10 -0400

    removed unused import

--- app/src/main/java/com/aa/draftt/auth/repositories/AuthResult.kt
+++ app/src/main/java/com/aa/draftt/auth/repositories/AuthResult.kt
@@ -1,6 +1,4 @@
 package com.aa.draftt.auth.repositories
 
-import com.google.firebase.auth.FirebaseUser
-
 // the user can be yeeted from this class
 data class AuthResult(val status: Boolean?, val error: String?)
