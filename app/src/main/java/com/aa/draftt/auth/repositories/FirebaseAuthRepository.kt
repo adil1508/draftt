348a032156926eee446580e566732336f94372d3
Adil Mian <adilmian95@gmail.com>
2021-04-28 23:43:11 -0400

    swithed login/signup functions to return their tasks + removed livedata + non-suspend currentuser fn

--- app/src/main/java/com/aa/draftt/auth/repositories/FirebaseAuthRepository.kt
+++ app/src/main/java/com/aa/draftt/auth/repositories/FirebaseAuthRepository.kt
@@ -2,6 +2,7 @@ package com.aa.draftt.auth.repositories
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.Dispatchers
@@ -11,66 +12,20 @@ import javax.inject.Inject
 
 class FirebaseAuthRepository @Inject constructor(private val firebaseAuth: FirebaseAuth) {
 
-    // Stores results of making login/signup calls
-    private val _authResult = MutableLiveData(
-        AuthResult(status = null, error = null)
-    )
-    val authResult: LiveData<AuthResult>
-        get() = _authResult
-
-    // Stores current logged in user
-    private val _firebaseUser = MutableLiveData(firebaseAuth.currentUser)
-    val firebaseUser: LiveData<FirebaseUser?>
-        get() = _firebaseUser
-
-    suspend fun login(email: String, password: String) {
-        withContext(Dispatchers.IO) {
-            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Timber.d("Successfully logged in user with email: $email")
-                    _firebaseUser.postValue(firebaseAuth.currentUser)
-                    _authResult.postValue(AuthResult(status = true, error = null))
-                } else {
-                    Timber.d("Failed to log in user with email: $email")
-                    _firebaseUser.postValue(null)
-                    _authResult.postValue(
-                        AuthResult(
-                            status = false,
-                            error = task.exception.toString()
-                        )
-                    )
-                }
-            }
+    suspend fun login(email: String, password: String): Task<com.google.firebase.auth.AuthResult> {
+        return withContext(Dispatchers.IO) {
+            firebaseAuth.signInWithEmailAndPassword(email, password)
         }
     }
 
-    suspend fun signup(email: String, password: String) {
-        withContext(Dispatchers.IO) {
+    suspend fun signup(email: String, password: String): Task<com.google.firebase.auth.AuthResult> {
+        return withContext(Dispatchers.IO) {
             firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        Timber.d("Successfully created user with email: $email")
-                        _firebaseUser.postValue(firebaseAuth.currentUser)
-                        _authResult.postValue(AuthResult(status = true, error = null))
-                    } else {
-                        Timber.d("FAILED to create user with email: $email")
-                        _firebaseUser.postValue(null)
-                        _authResult.postValue(
-                            AuthResult(
-                                status = false,
-                                error = task.exception.toString()
-                            )
-                        )
-                    }
-                }
         }
-
     }
 
-    suspend fun currentUser() {
-        withContext(Dispatchers.IO) {
-            _firebaseUser.postValue(firebaseAuth.currentUser)
-        }
+    fun currentUser(): FirebaseUser? {
+        return firebaseAuth.currentUser
     }
 
     // TODO: Add logging when implement this properly
