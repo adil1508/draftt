23f97fa20cec3b1dc2fbfce2f77a2d24032227f5
Adil Mian <adilmian95@gmail.com>
2021-04-28 23:51:15 -0400

    added impl-interface binding for auth

--- app/src/main/java/com/aa/draftt/hilt/DrafttModule.kt
+++ app/src/main/java/com/aa/draftt/hilt/DrafttModule.kt
@@ -1,8 +1,11 @@
 package com.aa.draftt.hilt
 
+import com.aa.draftt.auth.repositories.AuthRepository
+import com.aa.draftt.auth.repositories.FirebaseAuthRepository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -25,4 +28,9 @@ class DrafttProvidesModule {
 @InstallIn(SingletonComponent::class)
 abstract class DrafttBindingsModule {
     // for interfaces
+
+    @Binds
+    abstract fun bindAuthRepository(
+        firebaseAuthRepository: FirebaseAuthRepository
+    ): AuthRepository
 }
