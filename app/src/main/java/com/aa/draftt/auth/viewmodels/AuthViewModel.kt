65cd0f112b742b33982b76864780d8e22a92cf64
Adil Mian <adilmian95@gmail.com>
2021-04-28 23:43:40 -0400

    moved logic to check task success into viewmodel

--- app/src/main/java/com/aa/draftt/auth/viewmodels/AuthViewModel.kt
+++ app/src/main/java/com/aa/draftt/auth/viewmodels/AuthViewModel.kt
@@ -1,6 +1,7 @@
 package com.aa.draftt.auth.viewmodels
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aa.draftt.auth.repositories.AuthResult
@@ -9,18 +10,45 @@ import com.google.firebase.auth.FirebaseUser
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class AuthViewModel @Inject constructor(private val authRepository: FirebaseAuthRepository) :
     ViewModel() {
 
-    val authResult: LiveData<AuthResult> = authRepository.authResult
-    val user: LiveData<FirebaseUser?> = authRepository.firebaseUser
+    // Stores results of making login/signup calls
+    private val _authResult = MutableLiveData(
+        AuthResult(status = null, error = null)
+    )
+    val authResult: LiveData<AuthResult>
+        get() = _authResult
+
+    // Stores current logged in user
+    private val _firebaseUser = MutableLiveData<FirebaseUser>(currentUser())
+    val firebaseUser: LiveData<FirebaseUser?>
+        get() = _firebaseUser
 
     fun login(email: String, password: String) {
         viewModelScope.launch(Dispatchers.IO) {
-            authRepository.login(email, password)
+            val task = authRepository.login(email, password)
+            task.addOnCompleteListener {
+                if (task.isSuccessful) {
+                    Timber.d("Successfully logged in user with email: $email")
+                    _firebaseUser.postValue(it.result!!.user)
+                    _authResult.postValue(AuthResult(status = true, error = null))
+                } else {
+                    Timber.d("Failed to log in user with email: $email")
+                    _firebaseUser.postValue(null)
+                    _authResult.postValue(
+                        AuthResult(
+                            status = false,
+                            error = task.exception.toString()
+                        )
+                    )
+                }
+            }
         }
     }
 
@@ -30,10 +58,9 @@ class AuthViewModel @Inject constructor(private val authRepository: FirebaseAuth
         }
     }
 
-    fun currentUser() {
-        viewModelScope.launch(Dispatchers.IO) {
-            authRepository.currentUser()
-        }
+    // updates the current user live data
+    private fun currentUser(): FirebaseUser?{
+        return authRepository.currentUser()
     }
 
 }
