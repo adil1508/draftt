973903419187fc01fa374c93c2be9fb6347dffdd
Adil Mian <adilmian95@gmail.com>
2021-05-03 23:40:10 -0400

    setup database with users table

--- app/src/main/java/com/aa/draftt/room/DrafttDatabase.kt
+++ app/src/main/java/com/aa/draftt/room/DrafttDatabase.kt
@@ -1,4 +1,11 @@
 package com.aa.draftt.room
 
-class DrafttDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.aa.draftt.room.dao.UserDao
+import com.aa.draftt.room.models.User
+
+@Database(entities = arrayOf(User::class), version = 1)
+abstract class DrafttDatabase: RoomDatabase() {
+    abstract fun userDao(): UserDao
 }
