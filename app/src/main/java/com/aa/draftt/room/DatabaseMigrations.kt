a56e48f54bf5510c9d19734737406c5c2dfbf2d3
Adil Mian <adilmian95@gmail.com>
2021-05-05 00:58:42 -0400

    added migration to remove users table

---
+++ app/src/main/java/com/aa/draftt/room/DatabaseMigrations.kt
@@ -0,0 +1,16 @@
+package com.aa.draftt.room
+
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+class DatabaseMigrations {
+
+    companion object {
+
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("DROP TABLE users")
+            }
+        }
+    }
+}
