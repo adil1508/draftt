1e1a5cddf183ad6b09d95ef07db3f81185e0552a
Adil Mian <adilmian95@gmail.com>
2021-05-07 01:48:40 -0400

    moved models package

--- app/src/main/java/com/aa/draftt/models/AuthResult.kt
+++ app/src/main/java/com/aa/draftt/models/AuthResult.kt
@@ -1,4 +1,4 @@
-package com.aa.draftt.auth.repositories
+package com.aa.draftt.auth.models
 
 // the user can be yeeted from this class
 data class AuthResult(val status: Boolean?, val error: String?)
