cc26693f0fc7eaf7971decf97b67c44b0b0452b9
Adil Mian <adilmian95@gmail.com>
2021-05-07 03:01:48 -0400

    updated login function in viewmodel

--- app/src/main/java/com/aa/draftt/auth/viewmodels/AuthViewModel.kt
+++ app/src/main/java/com/aa/draftt/auth/viewmodels/AuthViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import com.aa.draftt.models.AuthResult
 import com.aa.draftt.models.UserModel
 import com.aa.draftt.auth.repositories.AuthRepository
+import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestoreException
@@ -35,7 +36,7 @@ class AuthViewModel @Inject constructor(private val authRepository: AuthReposito
         get() = _authResult
 
     // Store user info using our model
-    private val _user = MutableLiveData<UserModel>()
+    private val _user = MutableLiveData(UserModel())
     val user: LiveData<UserModel>
         get() = _user
 
@@ -51,26 +52,45 @@ class AuthViewModel @Inject constructor(private val authRepository: AuthReposito
 
 
     fun login(email: String, password: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            val task = authRepository.login(email, password)
-            task.addOnCompleteListener {
-                if (task.isSuccessful) {
-                    Timber.d("Successfully logged in user with email: $email")
-                    // Instead of posting the value of it.result!!.user, we need to get it from db
-                    _firebaseUser.postValue(it.result!!.user)
-                    _authResult.postValue(AuthResult(status = true, error = null))
 
-                } else {
-                    Timber.d("Failed to log in user with email: $email")
-                    _firebaseUser.postValue(null)
-                    _authResult.postValue(
-                        AuthResult(
-                            status = false,
-                            error = task.exception?.localizedMessage.toString()
-                        )
-                    )
-                }
+        /*
+        * Similar to Signup, first we attempt a login, then we get some info from fireStore
+        * */
+
+        viewModelScope.launch(Dispatchers.IO) {
+            val loginTask = authRepository.login(email, password)
+            try {
+                Tasks.await(loginTask)
+                Timber.d("Successfully logged in user with email: $email")
+            } catch (e: Exception) {
+                Timber.d("Failed to log in user with email: $email")
+                _error.postValue(loginTask.exception?.localizedMessage)
+                cancel()
             }
+
+            // At this point, we are successfully logged in,
+            // we need to get the Firestore record associated with this email
+            // NOTE: We are guaranteed only one record in Firestore cus otherwise,
+            // this user would've failed signup
+
+            // This task returns a max of 1 doc
+            val userDocTask = authRepository.getFromFirestore(email)
+            try {
+                val userDoc = Tasks.await(userDocTask).documents[0]
+                Timber.d("Successfully got user doc with email: $email")
+                _user.value?.id = userDoc.id
+                _user.value?.email = userDoc.data?.get("email") as String
+                _user.value?.name = userDoc.data?.get("name") as String
+            } catch (e: Exception) {
+                Timber.d("Failed to get user doc with email: $email")
+                _error.postValue(userDocTask.exception?.localizedMessage)
+                cancel()
+            }
+
+            // At this point we've setup our _user variable
+            // and we can navigate away :)
+            _navigateToAuthenticated.postValue(true)
+
         }
     }
 
@@ -81,7 +101,6 @@ class AuthViewModel @Inject constructor(private val authRepository: AuthReposito
         // secondly, make a call to FirebaseFirestore using email and name to create a record
 
         viewModelScope.launch(Dispatchers.IO) {
-            val userModel = UserModel()
             // Try to sign up using FirebaseAuth
             val signupTask = authRepository.signup(email = email, password = password)
             try {
@@ -103,9 +122,9 @@ class AuthViewModel @Inject constructor(private val authRepository: AuthReposito
                 Timber.d("Successfully stored user with name: $name, email: $email")
                 Timber.d("Document created with id: ${doc.id}")
                 val docData = Tasks.await(doc.get())
-                userModel.id = docData.id
-                userModel.name = docData.data?.get("name") as String?
-                userModel.email = docData.data?.get("email") as String?
+                _user.value?.id = docData.id
+                _user.value?.name = docData.data?.get("name") as String?
+                _user.value?.email = docData.data?.get("email") as String?
             } catch (e: FirebaseFirestoreException) {
                 // something went wrong writing user
                 Timber.d("Failed to write user with name: $name, email: $email")
@@ -115,7 +134,6 @@ class AuthViewModel @Inject constructor(private val authRepository: AuthReposito
 
             // At this point we've setup our _user variable
             // and we can navigate away :)
-            _user.postValue(userModel)
             _navigateToAuthenticated.postValue(true)
 
         }
