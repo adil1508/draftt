f0b647b34e554423a3bfe4ac7f1c094280582bf0
Adil Mian <adilmian95@gmail.com>
2021-05-07 01:51:35 -0400

    updated repos to add a firestore function to write user

--- app/src/main/java/com/aa/draftt/auth/repositories/AuthRepository.kt
+++ app/src/main/java/com/aa/draftt/auth/repositories/AuthRepository.kt
@@ -3,6 +3,7 @@ package com.aa.draftt.auth.repositories
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.DocumentReference
 
 interface AuthRepository {
 
@@ -10,7 +11,10 @@ interface AuthRepository {
 
     suspend fun signup(email: String, password: String): Task<AuthResult>
 
-    fun currentUser(): FirebaseUser?
+    // This will go into a FireStoreRepo
+    suspend fun writeToFirestore(name: String, email: String): Task<DocumentReference>
+
+    suspend fun currentUser(): FirebaseUser?
 
     // TODO: Add logging when implement this properly
     suspend fun signout()
--- app/src/main/java/com/aa/draftt/auth/repositories/FirebaseAuthRepository.kt
+++ app/src/main/java/com/aa/draftt/auth/repositories/FirebaseAuthRepository.kt
@@ -1,8 +1,11 @@
 package com.aa.draftt.auth.repositories
 
 import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -10,13 +13,13 @@ import javax.inject.Inject
 
 class FirebaseAuthRepository @Inject constructor(
     private val firebaseAuth: FirebaseAuth,
-    private val firebaseFirestore: FirebaseFirestore
+    private val firestore: FirebaseFirestore
 ) : AuthRepository {
 
     override suspend fun login(
         email: String,
         password: String
-    ): Task<com.google.firebase.auth.AuthResult> {
+    ): Task<AuthResult> {
         return withContext(Dispatchers.IO) {
             firebaseAuth.signInWithEmailAndPassword(email, password)
         }
@@ -25,15 +28,31 @@ class FirebaseAuthRepository @Inject constructor(
     override suspend fun signup(
         email: String,
         password: String
-    ): Task<com.google.firebase.auth.AuthResult> {
+    ): Task<AuthResult> {
         return withContext(Dispatchers.IO) {
             firebaseAuth.createUserWithEmailAndPassword(email, password)
         }
     }
 
-    override fun currentUser(): FirebaseUser? {
-        // Need to figure out if we want to return user from firebase or local cache
-        return firebaseAuth.currentUser
+    override suspend fun writeToFirestore(name: String, email: String): Task<DocumentReference> {
+
+        return withContext(Dispatchers.IO){
+            // write to firestore
+            val userData = hashMapOf(
+                "name" to name,
+                "email" to email
+            )
+            firestore.collection("users").add(userData)
+        }
+    }
+
+    override suspend fun currentUser(): FirebaseUser? {
+        // when we're asked for the current user, we should try to get
+        // firebase Auth user
+        // If that is not null, go to Firestore and grab the user info
+        return withContext(Dispatchers.IO){
+            firebaseAuth.currentUser
+        }
     }
 
     // TODO: Add logging when implement this properly
