f15670ac3fa9f79af454f8d02ced2dfae8f89530
Adil Mian <adilmian95@gmail.com>
2021-05-10 23:55:36 -0400

    added email checker utility

--- app/src/main/java/com/aa/draftt/Utils.kt
+++ app/src/main/java/com/aa/draftt/Utils.kt
@@ -1,6 +1,7 @@
 package com.aa.draftt
 
 import android.content.Context
+import android.text.TextUtils
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
@@ -11,6 +12,10 @@ class Utils {
         fun hideKeyboard(context: Context, rootView: View) {
             val imm = context.getSystemService(InputMethodManager::class.java)
             imm.hideSoftInputFromWindow(rootView.windowToken, 0)
+        }
+
+        fun isValidEmail(email: String): Boolean {
+            return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
         }
     }
 
