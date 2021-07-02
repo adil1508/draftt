09d160f6f49a76bfd3db6af8a710c7b752d7d6fd
Adil Mian <adilmian95@gmail.com>
2021-07-02 18:00:34 -0400

    initialize datastore in toplevel app file

--- app/src/main/java/com/aa/draftt/DrafttApplication.kt
+++ app/src/main/java/com/aa/draftt/DrafttApplication.kt
@@ -1,8 +1,14 @@
 package com.aa.draftt
 
 import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "DrafttDataStore")
 
 @HiltAndroidApp
 class DrafttApplication : Application() {
