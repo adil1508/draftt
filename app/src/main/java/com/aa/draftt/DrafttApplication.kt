8317715d61d93d2bf6ba7184c7ab46a27578ba26
Adil Mian <adilmian95@gmail.com>
2021-07-02 16:49:45 -0400

    renamed app name

--- app/src/main/java/com/aa/draftt/DrafttApplication.kt
+++ app/src/main/java/com/aa/draftt/DrafttApplication.kt
@@ -5,7 +5,7 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class App : Application() {
+class DrafttApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
